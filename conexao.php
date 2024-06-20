69316e1dcf9fc9055613df1f63879e3c5a5f8304
Italo Carvalho Sa <143663424+italocarvalhosa@users.noreply.github.com>
2024-06-19 21:57:37 -0300

    Atualizar o conexao.php

--- conexao.php
+++ conexao.php
@@ -1,19 +1,19 @@
 
 <?php
 
-$servername = "192.168.8.10";
+$servername = "192.168.8.10"; /* SSH
 
-$username = "grupophp03"; // Nome de usuário do MySQL
+$username = "grupophp03"; //  MySQL
 
-$password = "php03"; //  Sua senha do MySQL
+$password = "php03"; //   
 
 $dbname = "grupo03php"; //  Nome do seu banco de dados
  
-// Criar conexão
+// Criar conexão  e acessa os dados 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
  
-// Verificar conexão
+// Verificar conexão e se der erro o script é fechado 
 
 if ($conn->connect_error) {
 
