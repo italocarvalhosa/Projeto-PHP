01dea81dc6de92a999dc9f5c7034836599bb12ba
Italo Carvalho Sa <143663424+italocarvalhosa@users.noreply.github.com>
2024-06-19 21:19:01 -0300

    Atualizar o Bem_vindo.php

--- Bem_vindo.php
+++ Bem_vindo.php
@@ -1,8 +1,8 @@
 <?php
 
 session_start();
-if (!isset($_SESSION ['username'])){
-    header("Location: login.php");
+if (!isset($_SESSION ['username'])){ /* verificar se a variável está definida.*/
+    header("Location: login.php"); /* redireciona para a página login*/
     exit();
 }
 ?>
@@ -14,7 +14,7 @@ if (!isset($_SESSION ['username'])){
         <title>Bem vindo</title>
     </head>
     <body>
-        <h1>Bem Vindo <?php echo $_SESSION['username']; ?></h1>
-        <a href="logout.php">Sair</a>
+        <h1>Bem Vindo <?php echo $_SESSION['username']; ?></h1>/*imprimi uma mensagem da sessão*/ 
+        <a href="logout.php">Sair</a> 
     </body>
 </html>
