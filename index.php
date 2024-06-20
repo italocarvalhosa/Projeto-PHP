4ba41c6ddcc8fbc1ac5ff96fb37cbbe9d48997d4
Italo Carvalho Sa <143663424+italocarvalhosa@users.noreply.github.com>
2024-06-19 23:02:48 -0300

    Atualizar o index.php

--- index.php
+++ index.php
@@ -9,22 +9,23 @@ include "conexao.php";
 
 
 
-// Verificar se o ID da tarefa foi enviado
+// Verificar se o ID da tarefa foi enviado e o código é executado quando houver um post
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_GET['id'])) {
     // Receber o ID da tarefa
     $idTarefa = $_GET['id'];
 
     // Atualizar o status da tarefa para 'concluída' no banco de dados
     $sql = "UPDATE tarefas SET status = 'concluída' WHERE id = $idTarefa";
-    
+
+
+    // o código é executado e retorna true caso aconteça um erro com a consulta a conexão e fechada 
     if ($conn->query($sql) === TRUE) {
-        // Você pode adicionar outras ações aqui, se necessário
     } else {
         echo "Erro ao marcar tarefa como concluída: " . $conn->error;
     }
 }
 
-// Consulta SQL para selecionar todas as tarefas
+// seleciona a tabela e armazena os resultados para uma impressão 
 $sql = "SELECT * FROM tarefas";
 $result = $conn->query($sql);
 ?>
