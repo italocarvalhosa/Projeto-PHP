3de14b8c81c9a8c37e1926fdc89595529a4dec78
italo <italo.sa01@etec.sp.gov.br>
2024-06-20 20:19:27 -0300

    correção

--- concluirTarefa.php
+++ concluirTarefa.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'Dao.php';
 $id = $_GET['id_tarefa']; /* metodo superglobal para coleta de dados,
- alem de ser um array associativo para passar dados via url entre script e o php*/
+ alem de ser um array associativo para passar dados via url entre paginas web e script php*/
 
 /* instacia o dao e chama o metodo e o id como parametro*/
 $dao = new Dao();
