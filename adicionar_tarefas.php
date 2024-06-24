6e9f7e9035e63514ffba88f80713bbf01d33595e
brayan cavalcante <brayan.cavalcante@etec.sp.gov.br>
2024-06-24 19:53:07 -0300

    centralização

--- adicionar_tarefas.php
+++ adicionar_tarefas.php
@@ -9,6 +9,9 @@
   /* Exemplo de estilização personalizada */
   body {
     font-family: Arial, sans-serif;
+    text-align: center;
+
+   
   }
 </style>
 
@@ -16,7 +19,7 @@
 
 
 <h1>Adicionar Nova Tarefa</h1>
- 
+   
     <form action="processar_tarefas.php" method="post">
         <label for="nome">Nome da Tarefa:</label><br>
         <input type="text" id="nome" name="nome" required><br>
